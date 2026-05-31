818b4451c3e175f6815195c977faa4148d09fe9c
Zitronenjoghurt <117850085+Zitronenjoghurt@users.noreply.github.com>
2026-05-31 14:11:23 +0200

    implement codec for tuples and more

--- bit-codec/src/lib.rs
+++ bit-codec/src/lib.rs
@@ -1,8 +1,21 @@
 use crate::var_len::VarLen;
 pub use dec::{BitDecode, BitReader};
 pub use enc::{BitEncode, BitWriter};
+use std::borrow::Cow;
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet, LinkedList, VecDeque};
+use std::hash::Hash;
 use std::io::{self, Read, Write};
+use std::marker::PhantomData;
 use std::mem::MaybeUninit;
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::num::{
+    NonZeroI128, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroU128, NonZeroU16, NonZeroU32,
+    NonZeroU64, NonZeroU8, Wrapping,
+};
+use std::ops::{Range, RangeInclusive};
+use std::rc::Rc;
+use std::sync::Arc;
+use std::time::Duration;
 
 #[cfg(feature = "derive")]
 pub use bit_codec_derive::{BitDecode, BitEncode};
@@ -116,6 +129,38 @@ impl<T: BitDecode> BitDecode for Option<T> {
     }
 }
 
+macro_rules! impl_tuple {
+    ($($T:ident),+) => {
+        impl<$($T: BitEncode),+> BitEncode for ($($T,)+) {
+            fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+                #[allow(non_snake_case)]
+                let ($($T,)+) = self;
+                $($T.encode(w)?;)+
+                Ok(())
+            }
+        }
+
+        impl<$($T: BitDecode),+> BitDecode for ($($T,)+) {
+            fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+                Ok(($($T::decode(r)?,)+))
+            }
+        }
+    };
+}
+
+impl_tuple!(A);
+impl_tuple!(A, B);
+impl_tuple!(A, B, C);
+impl_tuple!(A, B, C, D);
+impl_tuple!(A, B, C, D, E);
+impl_tuple!(A, B, C, D, E, F);
+impl_tuple!(A, B, C, D, E, F, G);
+impl_tuple!(A, B, C, D, E, F, G, H);
+impl_tuple!(A, B, C, D, E, F, G, H, I);
+impl_tuple!(A, B, C, D, E, F, G, H, I, J);
+impl_tuple!(A, B, C, D, E, F, G, H, I, J, K);
+impl_tuple!(A, B, C, D, E, F, G, H, I, J, K, L);
+
 impl BitEncode for bool {
     fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
         w.write_bits(*self as u8, 1)
@@ -148,5 +193,480 @@ impl BitEncode for &str {
     fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
         VarLen(self.len() as u32).encode(w)?;
         w.write_full_bytes(self.as_bytes())
+    }
+}
+
+impl BitEncode for str {
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        w.write(&self)
+    }
+}
+
+impl BitEncode for () {
+    #[inline]
+    fn encode<W: Write>(&self, _w: &mut BitWriter<W>) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl BitDecode for () {
+    #[inline]
+    fn decode<R: Read>(_r: &mut BitReader<R>) -> io::Result<Self> {
+        Ok(())
+    }
+}
+
+impl<T: BitEncode + ?Sized> BitEncode for Box<T> {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        (**self).encode(w)
+    }
+}
+
+impl<T: BitDecode> BitDecode for Box<T> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        T::decode(r).map(Box::new)
+    }
+}
+
+impl<T: BitDecode> BitDecode for Box<[T]> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        Vec::<T>::decode(r).map(Vec::into_boxed_slice)
+    }
+}
+
+impl BitDecode for Box<str> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        String::decode(r).map(String::into_boxed_str)
+    }
+}
+
+impl<'a, T> BitEncode for Cow<'a, [T]>
+where
+    T: Clone + BitEncode,
+{
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.as_ref().encode(w)
+    }
+}
+
+impl<'a, T> BitDecode for Cow<'a, [T]>
+where
+    T: Clone + BitDecode,
+{
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        Vec::<T>::decode(r).map(Cow::Owned)
+    }
+}
+
+impl<'a> BitEncode for Cow<'a, str> {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.as_ref().encode(w)
+    }
+}
+
+impl<'a> BitDecode for Cow<'a, str> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        String::decode(r).map(Cow::Owned)
+    }
+}
+
+impl<T: BitEncode + ?Sized> BitEncode for Rc<T> {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        (**self).encode(w)
+    }
+}
+
+impl<T: BitDecode> BitDecode for Rc<T> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        T::decode(r).map(Rc::new)
+    }
+}
+
+impl<T: BitEncode + ?Sized> BitEncode for Arc<T> {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        (**self).encode(w)
+    }
+}
+
+impl<T: BitDecode> BitDecode for Arc<T> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        T::decode(r).map(Arc::new)
+    }
+}
+
+impl<T: BitEncode, E: BitEncode> BitEncode for Result<T, E> {
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        match self {
+            Ok(v) => {
+                w.write(&true)?;
+                v.encode(w)
+            }
+            Err(e) => {
+                w.write(&false)?;
+                e.encode(w)
+            }
+        }
+    }
+}
+
+impl<T: BitDecode, E: BitDecode> BitDecode for Result<T, E> {
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        if r.read::<bool>()? {
+            Ok(Ok(T::decode(r)?))
+        } else {
+            Ok(Err(E::decode(r)?))
+        }
+    }
+}
+
+impl<T> BitEncode for PhantomData<T> {
+    #[inline]
+    fn encode<W: Write>(&self, _w: &mut BitWriter<W>) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl<T> BitDecode for PhantomData<T> {
+    #[inline]
+    fn decode<R: Read>(_r: &mut BitReader<R>) -> io::Result<Self> {
+        Ok(PhantomData)
+    }
+}
+
+impl<T: BitEncode> BitEncode for Wrapping<T> {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.0.encode(w)
+    }
+}
+
+impl<T: BitDecode> BitDecode for Wrapping<T> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        T::decode(r).map(Wrapping)
+    }
+}
+
+macro_rules! impl_nonzero {
+    ($nz:ty, $inner:ty) => {
+        impl BitEncode for $nz {
+            #[inline]
+            fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+                self.get().encode(w)
+            }
+        }
+
+        impl BitDecode for $nz {
+            #[inline]
+            fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+                let val = <$inner>::decode(r)?;
+                <$nz>::new(val).ok_or_else(|| {
+                    io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        concat!(stringify!($nz), " cannot be zero"),
+                    )
+                })
+            }
+        }
+    };
+}
+
+impl_nonzero!(NonZeroU8, u8);
+impl_nonzero!(NonZeroU16, u16);
+impl_nonzero!(NonZeroU32, u32);
+impl_nonzero!(NonZeroU64, u64);
+impl_nonzero!(NonZeroU128, u128);
+impl_nonzero!(NonZeroI8, i8);
+impl_nonzero!(NonZeroI16, i16);
+impl_nonzero!(NonZeroI32, i32);
+impl_nonzero!(NonZeroI64, i64);
+impl_nonzero!(NonZeroI128, i128);
+
+macro_rules! impl_seq_collection {
+    ($ty:ident < T $(: $bound:ident)* >) => {
+        impl<T: BitEncode> BitEncode for $ty<T> {
+            fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+                VarLen(self.len() as u32).encode(w)?;
+                for item in self {
+                    item.encode(w)?;
+                }
+                Ok(())
+            }
+        }
+
+        impl<T: BitDecode $(+ $bound)*> BitDecode for $ty<T> {
+            fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+                let len = VarLen::decode(r)?.0 as usize;
+                let mut col = $ty::new();
+                for _ in 0..len {
+                    col.push_back(T::decode(r)?);
+                }
+                Ok(col)
+            }
+        }
+    };
+}
+
+impl_seq_collection!(VecDeque<T>);
+impl_seq_collection!(LinkedList<T>);
+
+impl<T: BitEncode> BitEncode for HashSet<T> {
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        VarLen(self.len() as u32).encode(w)?;
+        for item in self {
+            item.encode(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl<T: BitDecode + Eq + Hash> BitDecode for HashSet<T> {
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let len = VarLen::decode(r)?.0 as usize;
+        let mut set = HashSet::with_capacity(len);
+        for _ in 0..len {
+            set.insert(T::decode(r)?);
+        }
+        Ok(set)
+    }
+}
+
+impl<T: BitEncode> BitEncode for BTreeSet<T> {
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        VarLen(self.len() as u32).encode(w)?;
+        for item in self {
+            item.encode(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl<T: BitDecode + Ord> BitDecode for BTreeSet<T> {
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let len = VarLen::decode(r)?.0 as usize;
+        let mut set = BTreeSet::new();
+        for _ in 0..len {
+            set.insert(T::decode(r)?);
+        }
+        Ok(set)
+    }
+}
+
+impl<K: BitEncode, V: BitEncode> BitEncode for HashMap<K, V> {
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        VarLen(self.len() as u32).encode(w)?;
+        for (k, v) in self {
+            k.encode(w)?;
+            v.encode(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl<K: BitDecode + Eq + Hash, V: BitDecode> BitDecode for HashMap<K, V> {
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let len = VarLen::decode(r)?.0 as usize;
+        let mut map = HashMap::with_capacity(len);
+        for _ in 0..len {
+            map.insert(K::decode(r)?, V::decode(r)?);
+        }
+        Ok(map)
+    }
+}
+
+impl<K: BitEncode, V: BitEncode> BitEncode for BTreeMap<K, V> {
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        VarLen(self.len() as u32).encode(w)?;
+        for (k, v) in self {
+            k.encode(w)?;
+            v.encode(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl<K: BitDecode + Ord, V: BitDecode> BitDecode for BTreeMap<K, V> {
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let len = VarLen::decode(r)?.0 as usize;
+        let mut map = BTreeMap::new();
+        for _ in 0..len {
+            map.insert(K::decode(r)?, V::decode(r)?);
+        }
+        Ok(map)
+    }
+}
+
+impl BitEncode for char {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        (*self as u32).encode(w)
+    }
+}
+
+impl BitDecode for char {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let code = u32::decode(r)?;
+        char::from_u32(code)
+            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "invalid char codepoint"))
+    }
+}
+
+impl BitEncode for Duration {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.as_secs().encode(w)?;
+        self.subsec_nanos().encode(w)
+    }
+}
+
+impl BitDecode for Duration {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let secs = u64::decode(r)?;
+        let nanos = u32::decode(r)?;
+        if nanos >= 1_000_000_000 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Duration nanos >= 1 billion",
+            ));
+        }
+        Ok(Duration::new(secs, nanos))
+    }
+}
+
+impl BitEncode for Ipv4Addr {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        w.write_full_bytes(&self.octets())
+    }
+}
+
+impl BitDecode for Ipv4Addr {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let mut buf = [0u8; 4];
+        r.read_full_bytes(&mut buf)?;
+        Ok(Ipv4Addr::from(buf))
+    }
+}
+
+impl BitEncode for Ipv6Addr {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        w.write_full_bytes(&self.octets())
+    }
+}
+
+impl BitDecode for Ipv6Addr {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let mut buf = [0u8; 16];
+        r.read_full_bytes(&mut buf)?;
+        Ok(Ipv6Addr::from(buf))
+    }
+}
+
+impl BitEncode for SocketAddrV4 {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.ip().encode(w)?;
+        self.port().encode(w)
+    }
+}
+
+impl BitDecode for SocketAddrV4 {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let ip = Ipv4Addr::decode(r)?;
+        let port = u16::decode(r)?;
+        Ok(SocketAddrV4::new(ip, port))
+    }
+}
+
+impl BitEncode for SocketAddrV6 {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.ip().encode(w)?;
+        self.port().encode(w)
+    }
+}
+
+impl BitDecode for SocketAddrV6 {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let ip = Ipv6Addr::decode(r)?;
+        let port = u16::decode(r)?;
+        Ok(SocketAddrV6::new(ip, port, 0, 0))
+    }
+}
+
+impl BitEncode for SocketAddr {
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        match self {
+            SocketAddr::V4(a) => {
+                w.write_bits(0u8, 1)?;
+                a.encode(w)
+            }
+            SocketAddr::V6(a) => {
+                w.write_bits(1u8, 1)?;
+                a.encode(w)
+            }
+        }
+    }
+}
+
+impl BitDecode for SocketAddr {
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        match r.read_bits::<u8>(1)? {
+            0 => SocketAddrV4::decode(r).map(SocketAddr::V4),
+            _ => SocketAddrV6::decode(r).map(SocketAddr::V6),
+        }
+    }
+}
+
+impl<T: BitEncode> BitEncode for Range<T> {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.start.encode(w)?;
+        self.end.encode(w)
+    }
+}
+
+impl<T: BitDecode> BitDecode for Range<T> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let start = T::decode(r)?;
+        let end = T::decode(r)?;
+        Ok(start..end)
+    }
+}
+
+impl<T: BitEncode> BitEncode for RangeInclusive<T> {
+    #[inline]
+    fn encode<W: Write>(&self, w: &mut BitWriter<W>) -> io::Result<()> {
+        self.start().encode(w)?;
+        self.end().encode(w)
+    }
+}
+
+impl<T: BitDecode> BitDecode for RangeInclusive<T> {
+    #[inline]
+    fn decode<R: Read>(r: &mut BitReader<R>) -> io::Result<Self> {
+        let start = T::decode(r)?;
+        let end = T::decode(r)?;
+        Ok(start..=end)
     }
 }
--- bit-codec/src/tests.rs
+++ bit-codec/src/tests.rs
@@ -9,7 +9,7 @@ pub fn encode_to_vec<T: BitEncode>(v: &T) -> Vec<u8> {
     w.into_inner()
 }
 
-pub fn decode_from_bytes<T: BitDecode>(bytes: &[u8]) -> std::io::Result<T> {
+pub fn decode_from_bytes<T: BitDecode>(bytes: &[u8]) -> io::Result<T> {
     let mut r = BitReader::new(bytes);
     T::decode(&mut r)
 }
@@ -222,8 +222,8 @@ fn primitive_roundtrips() {
 
 #[test]
 fn bool_roundtrip() {
-    assert_eq!(roundtrip(&true), true);
-    assert_eq!(roundtrip(&false), false);
+    assert!(roundtrip(&true));
+    assert!(!roundtrip(&false));
 }
 
 #[test]
@@ -331,4 +331,451 @@ fn array_no_length_prefix() {
         "array should be smaller than vec (no length prefix)"
     );
     assert_eq!(arr_bytes, [1, 2]);
+}
+
+#[test]
+fn unit_roundtrip() {
+    assert_eq!(roundtrip(&()), ());
+}
+
+#[test]
+fn unit_zero_bytes() {
+    assert!(encode_to_vec(&()).is_empty());
+}
+
+// === Tuples ===
+
+#[test]
+fn tuple_1() {
+    assert_eq!(roundtrip(&(42u8,)), (42u8,));
+}
+
+#[test]
+fn tuple_2_heterogeneous() {
+    assert_eq!(
+        roundtrip(&(true, "hi".to_string())),
+        (true, "hi".to_string())
+    );
+}
+
+#[test]
+fn tuple_3() {
+    assert_eq!(roundtrip(&(1u8, 2u16, 3u32)), (1u8, 2u16, 3u32));
+}
+
+#[test]
+fn tuple_4() {
+    assert_eq!(
+        roundtrip(&(false, -1i32, "x".to_string(), std::f64::consts::PI)),
+        (false, -1i32, "x".to_string(), std::f64::consts::PI)
+    );
+}
+
+#[test]
+fn tuple_12() {
+    let val = (
+        1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8, 8u8, 9u8, 10u8, 11u8, 12u8,
+    );
+    assert_eq!(roundtrip(&val), val);
+}
+
+#[test]
+fn tuple_nested() {
+    let val = ((1u8, 2u16), (true, "nested".to_string()));
+    assert_eq!(roundtrip(&val), val);
+}
+
+// === Box ===
+
+#[test]
+fn box_roundtrip() {
+    assert_eq!(roundtrip(&Box::new(42u32)), Box::new(42u32));
+}
+
+#[test]
+fn box_str_roundtrip() {
+    let val: Box<str> = "hello".into();
+    assert_eq!(roundtrip(&val), val);
+}
+
+#[test]
+fn box_slice_roundtrip() {
+    let val: Box<[u16]> = vec![1, 2, 3].into_boxed_slice();
+    assert_eq!(roundtrip(&val), val);
+}
+
+#[test]
+fn box_nested() {
+    assert_eq!(
+        roundtrip(&Box::new(Box::new(99u8))),
+        Box::new(Box::new(99u8))
+    );
+}
+
+// === Cow ===
+
+#[test]
+fn cow_str_borrowed() {
+    let val: Cow<str> = Cow::Borrowed("borrowed");
+    assert_eq!(&*roundtrip(&val), "borrowed");
+}
+
+#[test]
+fn cow_str_owned() {
+    let val: Cow<str> = Cow::Owned("owned".to_string());
+    assert_eq!(&*roundtrip(&val), "owned");
+}
+
+#[test]
+fn cow_slice() {
+    let val: Cow<[u8]> = Cow::Owned(vec![10, 20, 30]);
+    assert_eq!(&*roundtrip(&val), &[10, 20, 30]);
+}
+
+// === Rc / Arc ===
+
+#[test]
+fn rc_roundtrip() {
+    let val = Rc::new(7u8);
+    assert_eq!(*roundtrip(&val), 7u8);
+}
+
+#[test]
+fn arc_roundtrip() {
+    let val = Arc::new("shared".to_string());
+    assert_eq!(*roundtrip(&val), "shared");
+}
+
+// === Result ===
+
+#[test]
+fn result_ok() {
+    let val: Result<u16, String> = Ok(42);
+    assert_eq!(roundtrip(&val), Ok(42));
+}
+
+#[test]
+fn result_err() {
+    let val: Result<u16, String> = Err("oops".into());
+    assert_eq!(roundtrip(&val), Err("oops".into()));
+}
+
+#[test]
+fn result_discriminant_is_one_bit() {
+    let ok: Result<(), ()> = Ok(());
+    let err: Result<(), ()> = Err(());
+    // both should encode to a single byte (1 bit + padding)
+    assert_eq!(encode_to_vec(&ok).len(), 1);
+    assert_eq!(encode_to_vec(&err).len(), 1);
+}
+
+// === PhantomData ===
+
+#[test]
+fn phantom_roundtrip() {
+    let p: PhantomData<Vec<String>> = PhantomData;
+    roundtrip(&p);
+}
+
+#[test]
+fn phantom_zero_bytes() {
+    assert!(encode_to_vec(&PhantomData::<u64>).is_empty());
+}
+
+// === Wrapping ===
+
+#[test]
+fn wrapping_roundtrip() {
+    assert_eq!(roundtrip(&Wrapping(255u8)), Wrapping(255u8));
+    assert_eq!(roundtrip(&Wrapping(i64::MIN)), Wrapping(i64::MIN));
+}
+
+// === NonZero ===
+
+#[test]
+fn nonzero_roundtrip() {
+    assert_eq!(
+        roundtrip(&NonZeroU8::new(1).unwrap()),
+        NonZeroU8::new(1).unwrap()
+    );
+    assert_eq!(
+        roundtrip(&NonZeroU32::new(u32::MAX).unwrap()),
+        NonZeroU32::new(u32::MAX).unwrap()
+    );
+    assert_eq!(
+        roundtrip(&NonZeroI32::new(-1).unwrap()),
+        NonZeroI32::new(-1).unwrap()
+    );
+}
+
+#[test]
+fn nonzero_decode_zero_fails() {
+    let bytes = encode_to_vec(&0u8);
+    assert!(decode_from_bytes::<NonZeroU8>(&bytes).is_err());
+
+    let bytes = encode_to_vec(&0u32);
+    assert!(decode_from_bytes::<NonZeroU32>(&bytes).is_err());
+
+    let bytes = encode_to_vec(&0u64);
+    assert!(decode_from_bytes::<NonZeroU64>(&bytes).is_err());
+}
+
+// === char ===
+
+#[test]
+fn char_roundtrip() {
+    assert_eq!(roundtrip(&'A'), 'A');
+    assert_eq!(roundtrip(&'🦀'), '🦀');
+    assert_eq!(roundtrip(&'\0'), '\0');
+    assert_eq!(roundtrip(&'\u{10FFFF}'), '\u{10FFFF}');
+}
+
+#[test]
+fn char_decode_surrogate_fails() {
+    let bytes = encode_to_vec(&0xD800u32);
+    assert!(decode_from_bytes::<char>(&bytes).is_err());
+}
+
+#[test]
+fn char_decode_out_of_range_fails() {
+    let bytes = encode_to_vec(&0x110000u32);
+    assert!(decode_from_bytes::<char>(&bytes).is_err());
+}
+
+// === Duration ===
+
+#[test]
+fn duration_roundtrip() {
+    assert_eq!(
+        roundtrip(&Duration::new(123, 456_789)),
+        Duration::new(123, 456_789)
+    );
+    assert_eq!(roundtrip(&Duration::ZERO), Duration::ZERO);
+    assert_eq!(
+        roundtrip(&Duration::new(u64::MAX, 999_999_999)),
+        Duration::new(u64::MAX, 999_999_999)
+    );
+}
+
+#[test]
+fn duration_decode_bad_nanos_fails() {
+    // manually encode secs=0, nanos=1_000_000_000
+    let mut w = BitWriter::new(Vec::new());
+    0u64.encode(&mut w).unwrap();
+    1_000_000_000u32.encode(&mut w).unwrap();
+    w.flush().unwrap();
+    assert!(decode_from_bytes::<Duration>(&w.into_inner()).is_err());
+}
+
+// === Ipv4Addr ===
+
+#[test]
+fn ipv4_roundtrip() {
+    assert_eq!(roundtrip(&Ipv4Addr::LOCALHOST), Ipv4Addr::LOCALHOST);
+    assert_eq!(roundtrip(&Ipv4Addr::UNSPECIFIED), Ipv4Addr::UNSPECIFIED);
+    assert_eq!(
+        roundtrip(&Ipv4Addr::new(192, 168, 1, 1)),
+        Ipv4Addr::new(192, 168, 1, 1)
+    );
+}
+
+#[test]
+fn ipv4_is_4_bytes() {
+    assert_eq!(encode_to_vec(&Ipv4Addr::LOCALHOST).len(), 4);
+}
+
+// === Ipv6Addr ===
+
+#[test]
+fn ipv6_roundtrip() {
+    assert_eq!(roundtrip(&Ipv6Addr::LOCALHOST), Ipv6Addr::LOCALHOST);
+    let addr = Ipv6Addr::new(0xfe80, 0, 0, 0, 0, 0, 0, 1);
+    assert_eq!(roundtrip(&addr), addr);
+}
+
+#[test]
+fn ipv6_is_16_bytes() {
+    assert_eq!(encode_to_vec(&Ipv6Addr::LOCALHOST).len(), 16);
+}
+
+// === SocketAddr ===
+
+#[test]
+fn socket_addr_v4_roundtrip() {
+    let addr = SocketAddrV4::new(Ipv4Addr::new(10, 0, 0, 1), 8080);
+    assert_eq!(roundtrip(&addr), addr);
+}
+
+#[test]
+fn socket_addr_v6_roundtrip() {
+    let addr = SocketAddrV6::new(Ipv6Addr::LOCALHOST, 443, 0, 0);
+    assert_eq!(roundtrip(&addr), addr);
+}
+
+#[test]
+fn socket_addr_enum_roundtrip() {
+    let v4: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let v6: SocketAddr = "[::1]:443".parse().unwrap();
+    assert_eq!(roundtrip(&v4), v4);
+    assert_eq!(roundtrip(&v6), v6);
+}
+
+#[test]
+fn socket_addr_v4_smaller_than_v6() {
+    let v4: SocketAddr = "0.0.0.0:0".parse().unwrap();
+    let v6: SocketAddr = "[::]:0".parse().unwrap();
+    assert!(encode_to_vec(&v4).len() < encode_to_vec(&v6).len());
+}
+
+// === HashMap ===
+
+#[test]
+fn hashmap_roundtrip() {
+    let mut m = HashMap::new();
+    m.insert("a".to_string(), 1u32);
+    m.insert("b".to_string(), 2);
+    assert_eq!(roundtrip(&m), m);
+}
+
+#[test]
+fn hashmap_empty() {
+    let m: HashMap<u8, u8> = HashMap::new();
+    assert_eq!(roundtrip(&m), m);
+}
+
+// === BTreeMap ===
+
+#[test]
+fn btreemap_roundtrip() {
+    let mut m = BTreeMap::new();
+    m.insert(1u8, "one".to_string());
+    m.insert(2, "two".to_string());
+    m.insert(3, "three".to_string());
+    assert_eq!(roundtrip(&m), m);
+}
+
+#[test]
+fn btreemap_empty() {
+    let m: BTreeMap<String, u32> = BTreeMap::new();
+    assert_eq!(roundtrip(&m), m);
+}
+
+// === HashSet ===
+
+#[test]
+fn hashset_roundtrip() {
+    let s: HashSet<u32> = [1, 2, 3, 100].into_iter().collect();
+    assert_eq!(roundtrip(&s), s);
+}
+
+#[test]
+fn hashset_empty() {
+    let s: HashSet<u8> = HashSet::new();
+    assert_eq!(roundtrip(&s), s);
+}
+
+// === BTreeSet ===
+
+#[test]
+fn btreeset_roundtrip() {
+    let s: BTreeSet<i16> = [-10, 0, 10, 20].into_iter().collect();
+    assert_eq!(roundtrip(&s), s);
+}
+
+// === VecDeque ===
+
+#[test]
+fn vecdeque_roundtrip() {
+    let v: VecDeque<u8> = [10, 20, 30].into_iter().collect();
+    assert_eq!(roundtrip(&v), v);
+}
+
+#[test]
+fn vecdeque_empty() {
+    let v: VecDeque<u32> = VecDeque::new();
+    assert_eq!(roundtrip(&v), v);
+}
+
+#[test]
+fn vecdeque_preserves_order() {
+    let mut v = VecDeque::new();
+    v.push_front(1u8);
+    v.push_back(2);
+    v.push_front(0);
+    // logical order: [0, 1, 2]
+    let decoded = roundtrip(&v);
+    assert_eq!(decoded.iter().copied().collect::<Vec<_>>(), vec![0, 1, 2]);
+}
+
+// === LinkedList ===
+
+#[test]
+fn linked_list_roundtrip() {
+    let l: LinkedList<String> = ["a", "b", "c"].iter().map(|s| s.to_string()).collect();
+    assert_eq!(roundtrip(&l), l);
+}
+
+#[test]
+fn linked_list_empty() {
+    let l: LinkedList<u8> = LinkedList::new();
+    assert_eq!(roundtrip(&l), l);
+}
+
+// === Range ===
+
+#[test]
+fn range_roundtrip() {
+    assert_eq!(roundtrip(&(10u32..20u32)), 10..20);
+    assert_eq!(roundtrip(&(-5i8..5i8)), -5..5);
+}
+
+#[test]
+fn range_inclusive_roundtrip() {
+    assert_eq!(roundtrip(&(1u16..=100u16)), 1..=100);
+    assert_eq!(roundtrip(&(0i32..=0i32)), 0..=0);
+}
+
+// === Composites / integration ===
+
+#[test]
+fn option_box_string() {
+    let val: Option<Box<String>> = Some(Box::new("boxed".into()));
+    assert_eq!(roundtrip(&val), val);
+}
+
+#[test]
+fn vec_of_results() {
+    let v: Vec<Result<u8, String>> = vec![Ok(1), Err("bad".into()), Ok(3)];
+    assert_eq!(roundtrip(&v), v);
+}
+
+#[test]
+fn map_of_ranges() {
+    let mut m = BTreeMap::new();
+    m.insert("a".to_string(), 0u32..10);
+    m.insert("b".to_string(), 100..200);
+    assert_eq!(roundtrip(&m), m);
+}
+
+#[test]
+fn tuple_of_collections() {
+    let val: (Vec<u8>, HashSet<u16>, BTreeMap<String, bool>) = (
+        vec![1, 2, 3],
+        [10, 20].into_iter().collect(),
+        [("x".into(), true), ("y".into(), false)]
+            .into_iter()
+            .collect(),
+    );
+    assert_eq!(roundtrip(&val), val);
+}
+
+#[test]
+fn deeply_nested() {
+    #[allow(clippy::type_complexity)]
+    let val: Vec<Option<Result<Box<(u8, String)>, NonZeroU16>>> = vec![
+        Some(Ok(Box::new((42, "deep".into())))),
+        Some(Err(NonZeroU16::new(999).unwrap())),
+        None,
+    ];
+    assert_eq!(roundtrip(&val), val);
 }
